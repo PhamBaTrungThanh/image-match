7b87e6774d53ca71b7865d9be0d4c5c5f7ca81d0
Arthur <phambatrungthanh@gmail.com>
2019-10-12 09:27:41 +0000

    Fix queue

--- image_match/mongodb_driver.py
+++ image_match/mongodb_driver.py
@@ -1,7 +1,7 @@
 from .signature_database_base import SignatureDatabaseBase
 from .signature_database_base import normalized_distance
 from multiprocessing import cpu_count, Process, Queue
-from multiprocessing.managers import Queue as managerQueue
+
 import numpy as np
 
 
@@ -49,7 +49,7 @@ class SignatureMongo(SignatureDatabaseBase):
         if word_limit is None:
             word_limit = self.N
 
-        initial_q = managerQueue.Queue()
+        initial_q = Queue()
 
         [initial_q.put({field_name: rec[field_name]}) for field_name in self.index_names[:word_limit]]
 
